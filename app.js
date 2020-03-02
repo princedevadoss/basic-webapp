4add6eb1f09f25298a368e1ab5796f9113e5a982
PrinceDevadoss <prince.devadoss@freshworks.com>
2020-03-02 11:07:16 +0530

    Database connectivity added

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var bodyParser = require('body-parser');
 const app = express();
+
+var dbString = "mongodb+srv://prince:Sasikala2122@cluster0-8f79j.mongodb.net/apptest?retryWrites=true&w=majority";
 
 var port = process.env.PORT || 8081;
 
@@ -23,6 +26,90 @@ app.post('/hi', function(req, res, next) {
     var c = Number(ar['a']) + Number(ar['b']);
     console.log(c);
     res.send({c : c});
+});
+
+app.get('/content1', function(req,res,next) {
+    var name = req.query.name;
+    console.log('name ', name);
+    name= name.trim();
+    var t = mongoose.connect(dbString);
+    t.then(function(c) {
+        let Schema = mongoose.Schema;
+        let testSchema = new Schema({
+        name: String,
+        age: Number,
+        mark: Number
+        });
+        let test = mongoose.model('tests', testSchema);
+        console.log('name ', name);
+        test.find({name: name}, function(err, testContent) {
+            if(err) {
+                console.log('Credentials Error');
+                res.send('Error');
+            }
+            console.log(testContent);
+            res.send('Success');
+        })
+    })
+    .catch(function(err) {
+        res.send(err);
+    })
+});
+
+app.get('/content', function(req, res, next) {
+    var t = mongoose.connect(dbString);
+    t.then(function(c) {
+        if(!c) throw error;
+        let Schema = mongoose.Schema;
+        let testSchema = new Schema({
+        name: String,
+        age: Number,
+        mark: Number
+        });
+        let test = mongoose.model('tests', testSchema);
+        console.log(typeof Schema, typeof testSchema);
+        test.find({}, function(err, testContent) {
+            if(err) {
+                console.log('Credentials Error');
+                res.send('Error');
+            }
+            console.log(testContent);
+            res.send('Success');
+        })
+    }).catch(function(err) {
+        console.log('MongoDB Connectivity failed');
+        res.send('Error');
+    });
+});
+
+app.post('/create', function(req, res, next) {
+    var data = req.body;
+    var name = data.name;
+    var age = data.age;
+    var t = mongoose.connect(dbString);
+    t.then(function(c) {
+        let Schema = mongoose.Schema;
+        let testSchema = new Schema({
+        name: String,
+        age: Number,
+        mark: Number
+        });
+        let test = mongoose.model('tests', testSchema);
+        var p = new test();
+        p.name = function() {};
+        p.age = age;
+        p.save(function(err, saveObj) {
+            if(err) {
+                console.log('Insertion error');
+                res.send('Insertion Error');
+            }
+            res.send('Successfully Inserted');
+        });
+    })
+    .catch(function(err) {
+        console.log('Connection Error');
+        res.send('Connection Failure');
+    })
 });
 
 app.get('*', function(req, res) {
